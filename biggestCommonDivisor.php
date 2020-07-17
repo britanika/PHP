db4e17f943cbfa8dd2f1176db903f15e37eea606
britanika <sas4.ciric@gmail.com>
2020-07-17 12:00:56 +0200

    part 3

---
+++ biggestCommonDivisor.php
@@ -0,0 +1,14 @@
+<?php 
+/*The biggest common divisor for 2 numbers/Euclidean algorithm*/
+$a = readline('Enter the first number: ');
+$c=(int)$a;
+$tmp;
+$b = readline('Enter the second number: ');
+$d=(int)$b;
+while($d!=0){
+    $tmp=$c;
+    $c=$d;
+    $d=$tmp%$c;
+}
+echo "The biggest common divisor is ".$c;
+?>
