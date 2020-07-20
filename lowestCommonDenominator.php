3e2be04dc3fc5f4e44163a2d93a334a50b95159c
britanika <sas4.ciric@gmail.com>
2020-07-20 17:14:50 +0200

    part 4

---
+++ lowestCommonDenominator.php
@@ -0,0 +1,20 @@
+<?php 
+/*The lowest common denominator for 2 numbers/Euclidean algorithm*/
+function lowestCommonDenominator (){
+$a = readline('Enter the first number: ');
+$b = readline('Enter the second number: ');
+$c=(int)$a;
+$d=(int)$b;
+$v=$c*$d;
+$tmp;
+while($d!=0){
+    $tmp=$c;
+    $c=$d;
+    $d=$tmp%$c;
+}
+echo "The biggest common divisor is ".$c."\n";
+$s=$v/$c;
+echo "The lowest common denominator is ".$s;
+}
+lowestCommonDenominator();
+?>
