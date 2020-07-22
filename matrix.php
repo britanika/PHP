27e756cf0f43178a6d292c2ca1806c44247dbfbd
britanika <sas4.ciric@gmail.com>
2020-07-22 19:16:41 +0200

    part 5

---
+++ matrix.php
@@ -0,0 +1,20 @@
+<?php
+$a = readline();
+$b = readline();
+$c = (int) $a;
+$d = (int) $b;
+$matirx;
+for ($i = 0; $i < $c; $i ++) {
+    for ($j = 0; $j < $d; $j ++) {
+        $f = readline();
+        $r = (int) $f;
+        $matrix[$i][$j] = $r;
+    }
+}
+for ($i = 0; $i < $c; $i ++) {
+    for ($j = 0; $j < $d; $j ++) {
+        echo $matrix[$i][$j] . " ";
+    }
+    echo "\n";
+}
+?>
