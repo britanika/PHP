4ce3a57cb3b2a079dea6caa9e955b554911a1c0c
britanika <sas4.ciric@gmail.com>
2020-07-29 16:35:40 +0200

    part 6

---
+++ sumOfMainDiagonalOfMatrix.php
@@ -0,0 +1,32 @@
+<?php
+function mainMatrix(){
+$a = readline();
+$b = readline();
+$c = (int) $a;
+$d = (int) $b;
+$matrix=[];
+for ($i = 0; $i < $c; $i ++) {
+    for ($j = 0; $j < $d; $j ++) {
+        $f = readline();
+        $r = (int) $f;
+        $matrix[$i][$j] = $r;
+    }
+}
+for ($i = 0; $i < $c; $i ++) {
+    for ($j = 0; $j < $d; $j ++) {
+        echo $matrix[$i][$j] . " ";
+    }
+    echo "\n";
+}
+
+    $sumG=0;
+    for ($i = 0; $i < $c; $i ++) {
+        for ($j = 0; $j < $c; $j ++) {
+            if($i==$j){$sumG+=$matrix[$i][$j];}
+        }
+    }
+    return $sumG;
+}
+echo "The sum of the elements on the main diagonal is ".mainMatrix();
+?>
+
